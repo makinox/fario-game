f65edefcb34845782e3a922ea4234db1a3e85487
Jesus Bossa <x@jesusbossa.dev>
2022-01-23 15:27:48 -0500

    chore: add events to the boxes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import kaboom from 'kaboom';
 import pipeTopRightSide from '/assets/pipe-top-right-side.png';
 import pipeTopLeftSide from '/assets/pipe-top-left-side.png';
 import blueEvilShroom from '/assets/blue-evil-shroom.png';
+import marioStanding from '/assets/mario-standing.png';
 import blueSurprise from '/assets/blue-surprise.png';
 import evilShroom1 from '/assets/evil-shroom-1.png';
 import pipeRight from '/assets/pipe-right.png';
@@ -18,13 +19,15 @@ import block from '/assets/block.png';
 import coin from '/assets/coin.png';
 
 kaboom({
-  // background: [0, 0, 0],
-  scale: 1.5,
+  background: [158, 225, 255],
+  font: 'sinko',
+  scale: 1.3,
 });
 
 loadSprite('pipe-top-right-side', pipeTopRightSide);
 loadSprite('pipe-top-left-side', pipeTopLeftSide);
 loadSprite('blue-evil-shroom', blueEvilShroom);
+loadSprite('mario-standing', marioStanding);
 loadSprite('blue-surprise', blueSurprise);
 loadSprite('evil-shroom-1', evilShroom1);
 loadSprite('pipe-right', pipeRight);
@@ -39,19 +42,27 @@ loadSprite('brick', brick);
 loadSprite('block', block);
 loadSprite('coin', coin);
 
+layer('obj');
+layer('ui');
+layers(['obj', 'ui'], 'obj');
+
+const SCORE = 0;
+const MOVE_SPEED = 120;
+const JUMP_FORCE = 500;
+
 const map = [
-  '                                          ',
-  '                                          ',
-  '                                          ',
-  '                                          ',
-  '                                          ',
-  '                                          ',
-  '                                          ',
-  '       %  =*=%                            ',
-  '                                          ',
-  '                                  -+      ',
-  '                  ^  ^            ()      ',
-  '====================================    ==',
+  '                                      ',
+  '                                      ',
+  '                                      ',
+  '                                      ',
+  '                                      ',
+  '                                      ',
+  '                                      ',
+  '       %  =*=%                        ',
+  '                                      ',
+  '                              -+      ',
+  '                  ^  ^        ()      ',
+  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    xx',
 ];
 
 const levelCfg = {
@@ -60,15 +71,15 @@ const levelCfg = {
   '=': () => [sprite('block'), area(), solid()],
   x: () => [sprite('brick'), area(), solid()],
   $: () => [sprite('coin'), 'coin'],
-  '%': () => [sprite('question'), 'coin-surprise', area(), solid()],
-  '*': () => [sprite('question'), 'mushroom-surprise', area(), solid()],
+  '%': () => [sprite('question'), area(), solid(), 'coin-surprise'],
+  '*': () => [sprite('question'), area(), solid(), 'mushroom-surprise'],
   '}': () => [sprite('unboxed'), area(), solid()],
   '(': () => [sprite('pipe-left'), scale(0.5), area(), solid()],
   ')': () => [sprite('pipe-right'), scale(0.5), area(), solid()],
   '-': () => [sprite('pipe-top-left-side'), scale(0.5), area(), solid(), 'pipe'],
   '+': () => [sprite('pipe-top-right-side'), scale(0.5), area(), solid(), 'pipe'],
   '^': () => [sprite('evil-shroom-1'), area(), solid(), 'dangerous'],
-  '#': () => [sprite('mushroom'), 'mushroom', body()],
+  '#': () => [sprite('mushroom'), 'mushroom', body(), area()],
   '£': () => [sprite('blue-brick'), area(), solid(), scale(0.5)],
   z: () => [sprite('blue-block'), area(), solid(), scale(0.5)],
   '@': () => [sprite('blue-surprise'), area(), solid(), scale(0.5), 'coin-surprise'],
@@ -76,4 +87,85 @@ const levelCfg = {
   s: () => [sprite('blue-steel'), area(), solid(), scale(0.5)],
 };
 
-addLevel(map, levelCfg);
+const gameLevel = addLevel(map, levelCfg);
+
+add([text('level 0'), pos(60, 6)]);
+
+const scoreLabel = add([
+  text('0'),
+  pos(30, 6),
+  layer('ui'),
+  {
+    value: '0',
+  },
+]);
+
+const player = add([sprite('mario-standing'), pos(30, 0), body(), area(), big(), 'fario']);
+
+function big() {
+  let timer = 0;
+  let isBig = false;
+
+  return {
+    update() {
+      if (isBig) {
+        timer -= dt();
+        if (timer <= 0) {
+          this.smallify();
+        }
+      }
+    },
+    isBig() {
+      return isBig;
+    },
+    smallify() {
+      this.scale = vec2(1);
+      timer = 0;
+      isBig = false;
+    },
+    biggify(time = 0) {
+      this.scale = vec2(2);
+      timer = time;
+      isBig = true;
+    },
+  };
+}
+
+keyDown(['left', 'a'], () => {
+  player.move(-MOVE_SPEED, 0);
+});
+
+keyDown(['right', 'd'], () => {
+  player.move(MOVE_SPEED, 0);
+});
+
+keyDown('space', () => {
+  if (player.isGrounded()) {
+    player.jump(JUMP_FORCE);
+  }
+});
+
+onCollide('fario', 'coin-surprise', (fari, box, colision) => {
+  if (colision.isTop() && colision.target.is('coin-surprise')) {
+    gameLevel.spawn('$', box.gridPos.sub(0, 1));
+    destroy(box);
+    gameLevel.spawn('}', box.gridPos.sub(0, 0));
+  }
+});
+
+onCollide('fario', 'mushroom-surprise', (fari, box, colision) => {
+  if (colision.isTop() && colision.target.is('mushroom-surprise')) {
+    gameLevel.spawn('#', box.gridPos.sub(0, 1));
+    destroy(box);
+    gameLevel.spawn('}', box.gridPos.sub(0, 0));
+  }
+});
+
+onCollide('fario', 'mushroom', (fari, box, colision) => {
+  fari.biggify(6);
+  destroy(box);
+});
+
+action('mushroom', (obj) => {
+  obj.move(20, 0);
+});
